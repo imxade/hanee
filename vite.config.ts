bd7cae250368f7e874010c8ed6c0a78bd869f6a3
XADE <notadevel@gmail.com>
2026-03-31 13:38:36 +0530

    cleaned serwist config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import viteReact from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 import { nitro } from "nitro/vite"
 import { serwist } from "@serwist/vite"
-import crypto from "node:crypto"
 
 function coopCoepMiddleware() {
 	return {
@@ -76,7 +75,6 @@ export default defineConfig({
 			devOptions: {
 				enabled: true,
 			},
-			additionalPrecacheEntries: [{ url: "/", revision: crypto.randomUUID() }],
 			maximumFileSizeToCacheInBytes: 50 * 1024 * 1024, // 50MB
 		}),
 	],
